940aaf5afbd2f2c1acb2b4ee6fd059b1cdfd608b
Jon Nordby <jononor@gmail.com>
2017-01-22 18:10:35 +0100

    ReadCapacitivePin: Update to latest readCapacitivePin
    Increases sensitivity

--- components/ReadCapacitivePin.hpp
+++ components/ReadCapacitivePin.hpp
@@ -28,9 +28,8 @@ microflo_component */
 // The pin should have a 1+Megaohm resistor pulling
 //  it up to +5v.
 uint8_t readCapacitivePin(int pinToMeasure) {
-  // This is how you declare a variable which
-  //  will hold the PORT, PIN, and DDR registers
-  //  on an AVR
+
+  // Variables used to translate from Arduino to AVR pin naming
   volatile uint8_t* port;
   volatile uint8_t* ddr;
   volatile uint8_t* pin;
@@ -38,40 +37,50 @@ uint8_t readCapacitivePin(int pinToMeasure) {
   //  Arduino pin number to the AVR PORT, PIN, DDR,
   //  and which bit of those registers we care about.
   byte bitmask;
-  if ((pinToMeasure >= 0) && (pinToMeasure <= 7)){
-    port = &PORTD;
-    ddr = &DDRD;
-    bitmask = 1 << pinToMeasure;
-    pin = &PIND;
-  }
-  if ((pinToMeasure > 7) && (pinToMeasure <= 13)){
-    port = &PORTB;
-    ddr = &DDRB;
-    bitmask = 1 << (pinToMeasure - 8);
-    pin = &PINB;
-  }
-  if ((pinToMeasure > 13) && (pinToMeasure <= 19)){
-    port = &PORTC;
-    ddr = &DDRC;
-    bitmask = 1 << (pinToMeasure - 13);
-    pin = &PINC;
-  }
+  port = portOutputRegister(digitalPinToPort(pinToMeasure));
+  ddr = portModeRegister(digitalPinToPort(pinToMeasure));
+  bitmask = digitalPinToBitMask(pinToMeasure);
+  pin = portInputRegister(digitalPinToPort(pinToMeasure));
+
   // Discharge the pin first by setting it low and output
   *port &= ~(bitmask);
   *ddr  |= bitmask;
   delay(1);
-  // Make the pin an input WITHOUT the internal pull-up on
+
+  uint8_t SREG_old = SREG; //back up the AVR Status Register
+  // Prevent the timer IRQ from disturbing our measurement
+  noInterrupts();
+  // Make the pin an input with the internal pull-up on
   *ddr &= ~(bitmask);
-  // Now see how long the pin to get pulled up
-  int cycles = 16000;
-  for(int i = 0; i < cycles; i++){
-    if (*pin & bitmask){
-      cycles = i;
-      break;
-    }
-  }
+  *port |= bitmask;
+
+  // Now see how long the pin to get pulled up. This manual unrolling of the loop
+  // decreases the number of hardware cycles between each read of the pin,
+  // thus increasing sensitivity.
+  uint8_t cycles = 17;
+  if (*pin & bitmask) { cycles =  0;}
+  else if (*pin & bitmask) { cycles =  1;}
+  else if (*pin & bitmask) { cycles =  2;}
+  else if (*pin & bitmask) { cycles =  3;}
+  else if (*pin & bitmask) { cycles =  4;}
+  else if (*pin & bitmask) { cycles =  5;}
+  else if (*pin & bitmask) { cycles =  6;}
+  else if (*pin & bitmask) { cycles =  7;}
+  else if (*pin & bitmask) { cycles =  8;}
+  else if (*pin & bitmask) { cycles =  9;}
+  else if (*pin & bitmask) { cycles = 10;}
+  else if (*pin & bitmask) { cycles = 11;}
+  else if (*pin & bitmask) { cycles = 12;}
+  else if (*pin & bitmask) { cycles = 13;}
+  else if (*pin & bitmask) { cycles = 14;}
+  else if (*pin & bitmask) { cycles = 15;}
+  else if (*pin & bitmask) { cycles = 16;}
+
+  // End of timing-critical section; turn interrupts back on if they were on before, or leave them off if they were off before
+  SREG = SREG_old;
+
   // Discharge the pin again by setting it low and output
-  //  It's important to leave the pins low if you want to
+  //  It's important to leave the pins low if you want to 
   //  be able to touch more than 1 sensor at a time - if
   //  the sensor is left pulled high, when you touch
   //  two sensors, your body will transfer the charge between
@@ -97,7 +106,7 @@ public:
         if (in.isSetup()) {
             currentState = false;
             pin = 1;
-            touchedThreshold = 60;
+            touchedThreshold = 2;
         } else if (port == InPorts::trigger) {
             checkPin();
         } else if (port == InPorts::pin && in.isInteger()) {
