65b2690d819768e4a0fda0749058f7875415e998
Jon Nordby <jononor@gmail.com>
2017-12-30 16:55:06 +0100

    DigitalRead: Don't use isSetup

--- components/DigitalRead.hpp
+++ components/DigitalRead.hpp
@@ -3,34 +3,39 @@ name: DigitalRead
 description: "Read a boolean value from pin. Value is read on @trigger"
 inports:
   trigger:
-    type: all
+    type: bang
     description: ""
+    triggering: true
   pin:
-    type: all
+    type: integer
     description: ""
+    triggering: true
   pullup:
-    type: all
+    type: true
     description: ""
+    triggering: true
 outports:
   out:
-    type: all
+    type: bool
     description: ""
 microflo_component */
 class DigitalRead : public SingleOutputComponent {
 public:
+    DigitalRead()
+        : pin(-1)
+        , pullup(true)
+    {
+    }
+
     virtual void process(Packet in, MicroFlo::PortId port) {
-        // Note: have to match components.json
-        const int triggerPort = 0;
-        const int pinConfigPort = 1;
-        const int pullupConfigPort = 2;
-        if (in.isSetup()) {
-            setPinAndPullup(12, true); // defaults
-        } else if (port == triggerPort && in.isData()) {
-            bool isHigh = io->DigitalRead(pin);
+        using namespace DigitalReadPorts;
+
+        if (port == InPorts::trigger) {
+            const bool isHigh = io->DigitalRead(pin);
             send(Packet(isHigh));
-        } else if (port == pinConfigPort && in.isNumber()) {
+        } else if (port == InPorts::pin && in.isNumber()) {
             setPinAndPullup(in.asInteger(), pullup);
-        } else if (port == pullupConfigPort && in.isBool()) {
+        } else if (port == InPorts::pullup && in.isBool()) {
             setPinAndPullup(pin, in.asBool());
         }
     }
@@ -38,9 +43,11 @@ private:
     void setPinAndPullup(int newPin, bool newPullup) {
         pin = newPin;
         pullup = newPullup;
-        io->PinSetMode(pin, IO::InputPin);
-        io->PinSetPullup(pin, pullup ? IO::PullUp : IO::PullNone);
+        if (pin != -1) {
+            io->PinSetMode(pin, IO::InputPin);
+            io->PinSetPullup(pin, pullup ? IO::PullUp : IO::PullNone);
+        }
     }
-    MicroFlo::PortId pin;
+    int8_t pin;
     bool pullup;
 };
